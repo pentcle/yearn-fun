7310412a1995bc85a7d89bd1dba7dfb9f95a2e15
pentcle <pentacle@pentacle.xyz>
2022-03-14 22:48:14 +0000

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const Header = ({children}) => {
-    return (<header><Link to="/"><img src={children} alt="logo"/></Link></header>);
+    return (<header><Link to="/" id={"home"}><img src={children} alt="logo" name="home"/></Link></header>);
 };
 
 export default Header;
