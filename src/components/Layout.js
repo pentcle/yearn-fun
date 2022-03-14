ced6d21adbf55f9d95643fddfd8a5994354aeec8
pentcle <pentacle@pentacle.xyz>
2022-03-14 22:58:21 +0000

    footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Footer from "./Footer";
 
 const Layout = ({children}) => (<div className="App">
         {children}
+    <Footer/>
     </div>);
 
 export default Layout;
