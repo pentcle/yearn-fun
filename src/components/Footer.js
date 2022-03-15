fbfa78e996dd14825a55ff9e20093b293e98ed6c
pentcle <pentacle@pentacle.xyz>
2022-03-15 09:21:17 +0000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,8 @@ class Footer extends Component {
                rel="noopener noreferrer">yearn.finance</a>
             <a href={"https://twitter.com/iearnfinance"} target={"_blank"}
                rel="noopener noreferrer">@iearnfinance</a>
+            <a href={"https://github.com/pentcle/yearn-fun"} target={"_blank"}
+               rel="noopener noreferrer">github</a>
         </footer>);
     }
 }
